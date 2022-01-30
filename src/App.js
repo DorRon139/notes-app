298fb078c9d350a649f63866e5d75cebaf4425d4
Dor <dorron139@gmail.com>
2022-01-30 11:30:08 +0200

    add note

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import Footer from './components/Footer';
-import FormArea from './components/FormArea';
-import Header from './components/Header';
-import Note from './components/Note'
+import React from "react";
+import Footer from "./components/Footer";
+import FormArea from "./components/FormArea";
+import Header from "./components/Header";
+import Note from "./components/Note";
 
 const App = () => {
   return (
     <div>
-        <Header />
-        <FormArea />
-        <Note />
-        <Note />
-        <Note />
-        <Footer />
+      <Header />
+      <FormArea />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
     </div>
-);
+  );
 };
 
 export default App;
