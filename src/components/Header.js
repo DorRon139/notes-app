57dd44e6d4925108695a4e1e43a60095706244e8
Dor Ron <dorron139@gmail.com>
2022-08-13 18:55:57 +0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ import Paper from '@mui/material/Paper';
 const Header = () => {
   return (
     <Paper className='nav-bar-paper' square elevation={3}>
-        <img style={{width: "50px", height: "50px"}} src="https://mpng.subpng.com/20180713/tre/kisspng-react-native-web-application-javascript-google-scholar-logo-5b486117ca2a66.0620183815314701038281.jpg" />
-        <h1 className='site-name'>React Notes App</h1>        
+        <img style={{width: "50px", height: "50px"}} alt="logo" src="https://mpng.subpng.com/20180713/tre/kisspng-react-native-web-application-javascript-google-scholar-logo-5b486117ca2a66.0620183815314701038281.jpg" />
+        <h1 className='site-name'>React Notes App</h1> 
+        <span>Options</span>       
     </Paper>
     );
 };
